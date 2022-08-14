51bbbbaf12176e9589fb687830c55917bbb98698
yee_b@worksap.co.jp <yee_b@worksap.co.jp>
2022-08-15 04:56:02 +0800

    remove useless constructor

--- frontend/src/components/SettingComponent/SettingComponent.js
+++ frontend/src/components/SettingComponent/SettingComponent.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 class SettingComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className={"container"}>
