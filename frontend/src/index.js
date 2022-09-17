3e08a80e6fb757a4a4ab729342a090ad77d21769
yee_b@worksap.co.jp <yee_b@worksap.co.jp>
2022-09-17 17:13:56 +0800

    wip add searchbar, notification button and menu for appbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,6 @@ root.render(
             <BrowserRouter>
                 <App />
             </BrowserRouter>
-            ,
         </Provider>
     </SnackbarProvider>,
 );
--- frontend/src/pages/base-page.js
+++ frontend/src/pages/base-page.js
@@ -1,22 +1,57 @@
 import {
     AppBar,
+    Avatar,
     Backdrop,
     Button,
     CircularProgress,
+    IconButton,
+    InputAdornment,
+    Menu,
+    MenuItem,
+    TextField,
     Toolbar,
+    Tooltip,
 } from '@mui/material';
 import React from 'react';
 import { MainSelectors } from '../state/selectors';
 import { connect } from 'react-redux';
 import { MainActions } from '../state/actions';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AddIcon from '@mui/icons-material/Add';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 class BasePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menuAnchorElement: null,
+        };
+    }
+
     logoutUser = () => {
         // TODO remove user from backend using logoutAccount
         this.props.setToken(null);
         localStorage.removeItem('token');
         this.props.navigate('/login');
     };
+
+    searchAdornment = () => {
+        return (
+            <InputAdornment position="start">
+                <IconButton
+                    style={{ cursor: 'inherit' }}
+                    disableRipple
+                    edge="end"
+                >
+                    <SearchIcon />
+                </IconButton>
+            </InputAdornment>
+        );
+    }
 
     render() {
         return (
@@ -35,15 +70,88 @@ class BasePage extends React.Component {
                                 className={'app-bar-logo'}
                                 alt="readit logo"
                             />
-                            <Button
-                                disableRipple
-                                variant="contained"
-                                color="primary"
-                                onClick={() => this.logoutUser()}
-                                style={{ marginLeft: 'auto' }}
-                            >
-                                Logout
-                            </Button>
+                            <div style={{ margin: 'auto' }}>
+                                <TextField
+                                    className="app-bar-search-bar"
+                                    size="small"
+                                    label="Search Readit"
+                                    InputProps={{
+                                        startAdornment: this.searchAdornment(),
+                                    }}
+                                />
+                            </div>
+                            <div style={{ marginLeft: '16px' }}>
+                                <IconButton>
+                                    <NotificationsIcon />
+                                </IconButton>
+                            </div>
+                            <div style={{ marginLeft: '16px' }}>
+                                <Tooltip title="Account settings">
+                                    <Button
+                                        style={{ textTransform: 'none' }}
+                                        onClick={(e) => {
+                                            this.setState({
+                                                menuAnchorElement: e.currentTarget,
+                                            })
+                                        }}
+                                        disableRipple
+                                        variant='text'
+                                        size="small"
+                                    >
+                                        <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                                        <span style={{ marginLeft: '8px', marginRight: '8px    ' }}>Username</span>
+                                        <ExpandMoreIcon style={{ marginRight: '8px' }} />
+                                    </Button>
+                                </Tooltip>
+                                <Menu
+                                    anchorEl={this.state.menuAnchorElement}
+                                    id="account-menu"
+                                    open={this.state.menuAnchorElement}
+                                    onClose={() => { this.setState({ menuAnchorElement: null })}}
+                                    onClick={() => { this.setState({ menuAnchorElement: null })}}
+                                    PaperProps={{
+                                    elevation: 0,
+                                    sx: {
+                                        overflow: 'visible',
+                                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                        mt: 1.5,
+                                        '& .MuiAvatar-root': {
+                                        width: 32,
+                                        height: 32,
+                                        ml: -0.5,
+                                        mr: 1,
+                                        },
+                                        '&:before': {
+                                        content: '""',
+                                        display: 'block',
+                                        position: 'absolute',
+                                        top: 0,
+                                        right: 14,
+                                        width: 10,
+                                        height: 10,
+                                        bgcolor: 'background.paper',
+                                        transform: 'translateY(-50%) rotate(45deg)',
+                                        zIndex: 0,
+                                        },
+                                    },
+                                    style: {
+                                      width: '220px',
+                                    },
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <MenuItem>
+                                        <AccountBoxIcon style={{ marginRight: '8px' }} /> Profile
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <AddIcon style={{ marginRight: '8px' }} /> Create a community
+                                    </MenuItem>
+                                    <MenuItem onClick={() => this.logoutUser()}>
+                                        <LogoutIcon style={{ marginRight: '8px' }} /> Logout
+                                    </MenuItem>
+                                </Menu>
+                            </div>
                         </Toolbar>
                     </AppBar>
                 )}
