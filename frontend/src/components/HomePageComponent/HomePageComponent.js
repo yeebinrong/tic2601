9c2019a09831d1ca70b7f1190749bb0879f2b6d7
Hwee Teng <huiting.nus@gmail.com>
2022-09-25 01:12:29 +0800

    add homepage component

---
+++ frontend/src/components/HomePageComponent/HomePageComponent.js
@@ -0,0 +1,309 @@
+import React from 'react';
+import './HomePageComponent.scss';
+import { retrieveHomePagePosts } from '../../apis/app-api';
+import { MainActions } from '../../state/actions';
+import { MainSelectors } from '../../state/selectors';
+import { connect } from 'react-redux';
+import TabButton from '../TabButton';
+import MenuButton from '../MenuButton';
+import {
+    styled,
+    Box,
+    Stack,
+    Paper,
+    InputBase,
+    IconButton,
+    Divider,
+    Chip,
+} from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import CommentIcon from '@mui/icons-material/Comment';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import StarsIcon from '@mui/icons-material/Stars';
+import ForwardIcon from '@mui/icons-material/Forward';
+import CreateCommunityComponent from '../CreateCommunityComponent/CreateCommunityComponent';
+
+class HomePageComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isCreateCommunityDialogOpen: false,
+            posts: [],
+        };
+        if (props.isVerifyDone) {
+            this.props.setIsLoading(true);
+            retrieveHomePagePosts().then((res) => {
+                this.props.setIsLoading(false);
+                this.setState({
+                    posts: res.data.rows,
+                });
+            });
+        }
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if (nextProps.isVerifyDone && !this.props.isVerifyDone) {
+            this.props.setIsLoading(true);
+            retrieveHomePagePosts().then((res) => {
+                this.props.setIsLoading(false);
+                this.setState({
+                    posts: res.data.rows,
+                });
+            });
+        }
+        return true;
+    }
+
+    setIsCreateCommunityDialog = (value) => {
+        this.setState({
+            isCreateCommunityDialogOpen: value,
+        });
+    };
+
+    render() {
+        return (
+            <Grid container spacing={6} style={{ margin: '16px 160px' }}>
+                <Grid xs={9}>
+                    <Box sx={{ width: '100%' }}>
+                        <Stack spacing={2}>
+                            <Box>
+                                <Paper
+                                    component="form"
+                                    sx={{
+                                        p: '2px 4px',
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <IconButton
+                                        sx={{ p: '10px' }}
+                                        aria-label="profile"
+                                    >
+                                        <AccountCircleIcon />
+                                    </IconButton>
+                                    <InputBase
+                                        sx={{ ml: 1, flex: 1 }}
+                                        placeholder="Create Post"
+                                        inputProps={{
+                                            'aria-label': 'create a post',
+                                        }}
+                                    />
+                                    <Divider
+                                        sx={{ height: 28, m: 0.5 }}
+                                        orientation="vertical"
+                                    />
+                                    <IconButton
+                                        color="primary"
+                                        sx={{ p: '10px' }}
+                                        aria-label="photo"
+                                    >
+                                        <AddPhotoAlternateIcon />
+                                    </IconButton>
+                                    <IconButton
+                                        color="primary"
+                                        sx={{ p: '10px' }}
+                                        aria-label="attach"
+                                    >
+                                        <AttachFileIcon />
+                                    </IconButton>
+                                </Paper>
+                            </Box>
+                            <Item>
+                                <TabButton />
+                            </Item>
+                            {this.state.posts.map((post) => {
+                                {
+                                    console.log(post);
+                                }
+                                return (
+                                    <Item>
+                                        <Stack
+                                            spacing={1}
+                                            direction="column"
+                                            style={{ margin: '6px' }}
+                                        >
+                                            <Stack
+                                                direction="row"
+                                                divider={
+                                                    <Divider
+                                                        orientation="vertical"
+                                                        flexItem
+                                                    />
+                                                }
+                                                spacing={2}
+                                            >
+                                                <Box>
+                                                    <IconButton
+                                                        color="primary"
+                                                        sx={{ p: '10px' }}
+                                                        aria-label="stars"
+                                                    >
+                                                        <StarsIcon />
+                                                    </IconButton>
+                                                    r/{post.community_name}
+                                                </Box>
+                                                <Box
+                                                    style={{
+                                                        paddingTop: '11px',
+                                                    }}
+                                                >
+                                                    Posted by u/{post.user_name}
+                                                </Box>
+                                                <Box
+                                                    style={{
+                                                        paddingTop: '11px',
+                                                    }}
+                                                >
+                                                    {post.age.days
+                                                        ? post.age.days +
+                                                          ' days ago'
+                                                        : post.age.hours +
+                                                          ' hours ago'}
+                                                </Box>
+                                            </Stack>
+                                            <Stack
+                                                direction="row"
+                                                spacing={1}
+                                                style={{
+                                                    alignItems: 'center',
+                                                    textAlign: 'left',
+                                                    paddingLeft: '11px',
+                                                }}
+                                            >
+                                                <b>{post.title}</b>
+                                                <Chip
+                                                    label={post.flair_name}
+                                                    color="primary"
+                                                    variant="outlined"
+                                                    size="small"
+                                                    clickable={true}
+                                                />
+                                            </Stack>
+                                            <Stack direction="row" spacing={1}>
+                                                <Box>
+                                                    <IconButton
+                                                        sx={{ p: '10px' }}
+                                                        aria-label="upvote"
+                                                    >
+                                                        <ForwardIcon
+                                                            style={{
+                                                                transform:
+                                                                    'rotate(270deg)',
+                                                            }}
+                                                        />
+                                                    </IconButton>
+                                                    {post.favour_point}
+                                                    <IconButton
+                                                        sx={{ p: '10px' }}
+                                                        aria-label="downvote"
+                                                    >
+                                                        <ForwardIcon
+                                                            style={{
+                                                                transform:
+                                                                    'rotate(90deg)',
+                                                            }}
+                                                        />
+                                                    </IconButton>
+                                                </Box>
+                                                <Box>
+                                                    <IconButton
+                                                        sx={{ p: '10px' }}
+                                                        aria-label="comment"
+                                                    >
+                                                        <CommentIcon />
+                                                    </IconButton>
+                                                    {post.count} Comments
+                                                </Box>
+                                                <Box>
+                                                    <IconButton
+                                                        sx={{ p: '10px' }}
+                                                        aria-label="favourite"
+                                                    >
+                                                        <BookmarkIcon />
+                                                    </IconButton>
+                                                    Favourite
+                                                </Box>
+                                                <MenuButton />
+                                            </Stack>
+                                        </Stack>
+                                    </Item>
+                                );
+                            })}
+                        </Stack>
+                    </Box>
+                </Grid>
+                <Grid xs style={{ position: 'relative' }}>
+                    <Item style={{ padding: '16px' }}>
+                        <Stack spacing={2} direction="column">
+                            <Box style={{ textAlign: 'left' }}>
+                                <b>Home</b>
+                                <p>Your personal Readit homepage.</p>
+                                <p>
+                                    Come here to check in with your favourite
+                                    communities.
+                                </p>
+                            </Box>
+                            <Chip
+                                label="Create Post"
+                                color="primary"
+                                clickable={true}
+                            />
+                            <Chip
+                                label="Create Community"
+                                color="primary"
+                                variant="outlined"
+                                clickable={true}
+                                onClick={() =>
+                                    this.setIsCreateCommunityDialog(true)
+                                }
+                            />
+                            <CreateCommunityComponent
+                                open={this.state.isCreateCommunityDialogOpen}
+                                onClose={() =>
+                                    this.setIsCreateCommunityDialog(false)
+                                }
+                            />
+                        </Stack>
+                    </Item>
+                    <Box
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Chip
+                            label="Back to Top"
+                            color="primary"
+                            clickable={true}
+                            style={{
+                                position: 'absolute',
+                                bottom: '23px',
+                            }}
+                        />
+                    </Box>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#fff',
+    ...theme.typography.body2,
+    color: theme.palette.text.secondary,
+}));
+
+const mapStateToProps = (state) => ({
+    isVerifyDone: MainSelectors.getIsVerifyDone(state),
+    isLoading: MainSelectors.getIsLoading(state),
+});
+
+const mapDispatchToProps = {
+    setIsLoading: MainActions.setIsLoading,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePageComponent);
